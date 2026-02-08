4e88f27923b842757b7630d9fdd761bceb829f19
nawaabbro6-png <nawaabbro6@gmail.com>
2026-02-09 00:58:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,24 @@
 import os
 
-API_ID = API_ID = 22837269
+API_ID = int(os.environ.get("API_ID", 22518279))
 
-API_HASH = os.environ.get("API_HASH", "86737b9633320c818d029736dd07660b")
+API_HASH = os.environ.get(
+    "API_HASH",
+    "61e5cc94bc5e6318643707054e54caf4"
+)
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8492539241:AAH-LHWeucOBybzVmMss_Jh4mrRAIS2XD7c")
+BOT_TOKEN = os.environ.get(
+    "BOT_TOKEN",
+    "8403969795:AAHd_gYphmuPoD0_z3b38-JbHeH-lZhgIFA"
+)
 
-PASS_DB = int(os.environ.get("PASS_DB", "721"))
+PASS_DB = int(os.environ.get("PASS_DB", 721))
 
 OWNER = int(os.environ.get("OWNER", 7212452634))
-STRING = int(os.environ.get("STRING", AQFwCeIAOz1FS7JecfYU8zMZdNCoey8c3cbpOlG0CmPPY9mXBXyG2C0_Uf83cWS_dI38I16qpCyuggIpwc2LrcYQtUXEbyjxtfRWl3jif61NDbq95dMqvSLJkYz6xGaPas5qCfMubSwdkxgaFc_ejU1A5Fglp4RsPKnH_G4OjF_wEUAzNiWn0PhTVlUT-au6thdjfR-hsPvGUnksl4bTRb4BfR0cFsh0X8Tg14_KGJRMBGUN2URG1PO1ATGpiFDPmUDFco2SwQIW6VadpUUROTUD70z0SnAvQ7aZMD7MvpovTdHO8YJVu0GiMT5qJZKtNGHUzKj4rdxiKdHoA0_9z5h2pIR20QAAAAGrb7QLAA))
+
+STRING = os.environ.get(
+    "STRING",
+    "AQFwCeIAOz1FS7JecfYU8zMZdNCoey8c3cbpOlG0CmPPY9mXBXyG2C0_Uf83cWS_dI38I16qpCyuggIpwc2LrcYQtUXEbyjxtfRWl3jif61NDbq95dMqvSLJkYz6xGaPas5qCfMubSwdkxgaFc_ejU1A5Fglp4RsPKnH_G4OjF_wEUAzNiWn0PhTVlUT-au6thdjfR-hsPvGUnksl4bTRb4BfR0cFsh0X8Tg14_KGJRMBGUN2URG1PO1ATGpiFDPmUDFco2SwQIW6VadpUUROTUD70z0SnAvQ7aZMD7MvpovTdHO8YJVu0GiMT5qJZKtNGHUzKj4rdxiKdHoA0_9z5h2pIR20QAAAAGrb7QLAA"
+)
 
 LOG = -1002074904583
-
